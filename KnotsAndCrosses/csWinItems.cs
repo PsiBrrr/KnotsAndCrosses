da7a40b38104f1c32f5a9d671004fe5a9dd6bc7f
Andrew Fleet <mr.andrewjfleet@gmail.com>
2020-01-30 20:43:13 -0500

    New class
    Added class csWinItems with:
    Label
    Button

---
+++ KnotsAndCrosses/csWinItems.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace KnotsAndCrosses
+{
+    class csWinItems
+    {
+        public Label addLabel(Label lLabel, Point pLocation, Size sSize, String sText)
+        {
+            lLabel = new Label
+            {
+                Location = pLocation,
+                Size = sSize,
+                Text = sText
+            };
+            return lLabel;
+        }
+        public Label addLabel(Label lLabel, Point pLocation, Size sSize, String sText, Color cForeColor)
+        {
+            lLabel = new Label
+            {
+                Location = pLocation,
+                Size = sSize,
+                Text = sText,
+                ForeColor = cForeColor
+            };
+            return lLabel;
+        }
+        public Button addButton(Button bButton, Point pLocation, Size sSize, Int16 tTag, String sText, System.EventHandler click)
+        {
+            bButton = new Button
+            {
+                Location = pLocation,
+                Size = sSize,
+                Tag = tTag,
+                Text = sText
+            };
+            bButton.Click += click;
+            return bButton;
+        }
+        public Button addButton(Button bButton, Point pLocation, Size sSize, Int16 tTag, String sText, Color cBackColor, System.EventHandler click)
+        {
+            bButton = new Button
+            {
+                Location = pLocation,
+                Size = sSize,
+                Tag = tTag,
+                Text = sText,
+                BackColor = cBackColor
+            };
+            bButton.Click += click;
+            return bButton;
+        }
+    }
+}
